e871d936f8ca7cf3ad5a1d0cb3383e164779f308
Charlie Chen <charlie@jomo.network>
2023-10-06 14:47:49 -0700

    Debugging capture_all and non string values

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 fn find_ranges(json: &str, target_keys: &[&str]) -> Vec<[usize; 2]> {
+    let mut ranges = Vec::new();
+    let mut stack: Vec<String> = Vec::new();
     let mut in_string = false;
-    let mut skip_char = false;
     let mut start_idx: Option<usize> = None;
+    let mut skip_char = false;
+    let mut is_key = false;
     let mut brace_count = 0;
     let mut bracket_count = 0;
     let mut capture_all = false;
-    let mut stack: Vec<&str> = Vec::new();
-    let mut ranges: Vec<[usize; 2]> = Vec::new();
 
     for (i, c) in json.chars().enumerate() {
         if skip_char {
@@ -29,9 +30,6 @@ fn find_ranges(json: &str, target_keys: &[&str]) -> Vec<[usize; 2]> {
                     capture_all = false;
                 }
                 ranges.push([i, i + 1]);
-                if !in_string {
-                    stack.pop();
-                }
             }
             '[' => {
                 if stack == target_keys {
@@ -47,41 +45,83 @@ fn find_ranges(json: &str, target_keys: &[&str]) -> Vec<[usize; 2]> {
                     capture_all = false;
                 }
                 ranges.push([i, i + 1]);
+            }
+            ':' => {
                 if !in_string {
-                    stack.pop();
+                    ranges.push([i, i + 1]);
+                    is_key = false;
+                    // Capture the space after the colon
+                    if json[i + 1..]
+                        .chars()
+                        .next()
+                        .unwrap_or_default()
+                        .is_whitespace()
+                    {
+                        let space_length = json[i + 1..]
+                            .chars()
+                            .take_while(|&ch| ch.is_whitespace())
+                            .count();
+                        ranges.push([i + 1, i + 1 + space_length]);
+                    }
                 }
             }
             '"' => {
                 if capture_all {
                     if in_string {
                         ranges.push([start_idx.unwrap(), i + 1]);
-                        start_idx = None;
                     } else {
                         start_idx = Some(i);
                     }
                     in_string = !in_string;
                 } else {
                     if in_string {
-                        let key_or_value = &json[start_idx.unwrap() + 1..i];
-                        if stack.last() == Some(&key_or_value) {
-                            stack.pop();
-                        } else {
-                            stack.push(key_or_value);
-                        }
-                        ranges.push([start_idx.unwrap(), i + 1]);
+                        // End of string
+                        if let Some(start) = start_idx {
+                            if stack == target_keys || is_key {
+                                ranges.push([start, i + 1]);
+                            }
                             start_idx = None;
-                            start_idx = None;
-                        }
-                        start_idx = None;
                         }
                     } else {
+                        // Start of string
                         start_idx = Some(i);
+                        if let Some(next_double_quote) = json[i + 1..].find('"') {
+                            let next_double_quote = next_double_quote + i + 1;
+                            let content = &json[i + 1..next_double_quote];
+                            if json[next_double_quote + 1..]
+                                .chars()
+                                .next()
+                                .unwrap_or_default()
+                                .is_whitespace()
+                            {
+                                let next_relevant_char = json[next_double_quote + 1..]
+                                    .chars()
+                                    .skip_while(|&ch| ch.is_whitespace())
+                                    .next()
+                                    .unwrap_or_default();
+                                is_key = next_relevant_char == ':';
+                            } else {
+                                is_key = json[next_double_quote + 1..]
+                                    .chars()
+                                    .next()
+                                    .unwrap_or_default()
+                                    == ':';
+                            }
+                            if is_key {
+                                stack.push(content.to_string());
+                            }
+                            skip_char = true;
+                        } else {
+                            // If there is no closing double quote, end the loop to prevent invalid behavior
+                            break;
+                        }
                     }
                     in_string = !in_string;
                 }
             }
             ',' => {
                 if capture_all {
+                    // Capture the comma and the space after it if present
                     if let Some(next_char) = json.chars().nth(i + 1) {
                         if next_char.is_whitespace() {
                             ranges.push([i, i + 2]);
@@ -97,26 +137,75 @@ fn find_ranges(json: &str, target_keys: &[&str]) -> Vec<[usize; 2]> {
                     }
                 }
             }
+
+            // For non-string values
             ch if ch.is_numeric() || ch == '-' || ch == '.' => {
                 if capture_all {
                     if start_idx.is_none() {
                         start_idx = Some(i);
                     }
+                    // If it's the end of a non-string value, push it to ranges
+                    if let Some(next_char) = json[i + 1..].chars().next() {
+                        if next_char == ','
+                            || next_char == '}'
+                            || next_char == ']'
+                            || next_char.is_whitespace()
+                        {
+                            if let Some(start) = start_idx {
+                                ranges.push([start, i + 1]);
+                                start_idx = None;
+                            }
+                        }
+                    }
                 } else {
                     if start_idx.is_none() && stack == target_keys {
                         start_idx = Some(i);
                     }
+                    // If it's the end of a non-string value and it matches the target keys, push it to ranges
+                    if let Some(next_char) = json[i + 1..].chars().next() {
+                        if next_char == ','
+                            || next_char == '}'
+                            || next_char == ']'
+                            || next_char.is_whitespace()
+                        {
+                            if let Some(start) = start_idx {
+                                ranges.push([start, i + 1]);
+                                start_idx = None;
+                            }
+                        }
+                    }
                 }
             }
             _ => {
-                if capture_all && start_idx.is_some() {
-                    ranges.push([start_idx.unwrap(), i]);
-                    start_idx = None;
+                if !in_string && !capture_all {
+                    if start_idx.is_none() {
+                        start_idx = Some(i);
+                    }
+                    // If it's the end of a non-string value, push it to ranges
+                    if let Some(next_char) = json[i + 1..].chars().next() {
+                        if next_char == ','
+                            || next_char == '}'
+                            || next_char == ']'
+                            || next_char.is_whitespace()
+                        {
+                            if let Some(start) = start_idx {
+                                if stack == target_keys {
+                                    ranges.push([start, i + 1]);
+                                }
+                                start_idx = None;
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 
+    if let Some(start) = start_idx {
+        ranges.push([start, json.len()]);
+    }
+
+    // Merge consecutive ranges
     ranges.sort_by(|a, b| a[0].cmp(&b[0]));
     let mut merged_ranges: Vec<[usize; 2]> = Vec::new();
     for range in ranges {
